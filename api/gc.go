e4e22678b939f649a74c1713eb54c0a0c4e7878c
deniszh <denis.zhdanov@gmail.com>
2018-11-30 00:31:00 +0100

    Atoi

--- api/gc.go
+++ api/gc.go
@@ -20,7 +20,7 @@ func (s *Server) getNormalGCPercent(ctx *middleware.Context) {
 }
 
 func (s *Server) setStartupGCPercent(ctx *middleware.Context, percent models.StartupGCPercent) {
-	value, err := strconv.ParseInt(percent.Value, 10, 0)
+	value, err := strconv.Atoi(percent.Value)
 	if err != nil {
 		response.Write(ctx, response.NewError(http.StatusBadRequest, fmt.Sprintf(
 			"could not parse status to Int. %s",
@@ -33,7 +33,7 @@ func (s *Server) setStartupGCPercent(ctx *middleware.Context, percent models.Sta
 }
 
 func (s *Server) setNormalGCPercent(ctx *middleware.Context, percent models.NormalGCPercent) {
-	value, err := strconv.ParseInt(percent.Value, 10, 0)
+	value, err := strconv.Atoi(percent.Value)
 	if err != nil {
 		response.Write(ctx, response.NewError(http.StatusBadRequest, fmt.Sprintf(
 			"could not parse status to Int. %s",
