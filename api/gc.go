729e375da4fe8d2edbfcf445d661edf37b0e3521
deniszh <denis.zhdanov@gmail.com>
2018-12-28 12:38:02 +0100

    Oops 2

--- api/gc.go
+++ api/gc.go
@@ -16,14 +16,14 @@ func (s *Server) getNormalGCPercent(ctx *middleware.Context) {
 }
 
 func (s *Server) setStartupGCPercent(ctx *middleware.Context, percent models.StartupGCPercent) {
-	kafkamdm.GoGCmutex.lock()
+	kafkamdm.GoGCmutex.Lock()
 	defer kafkamdm.GoGCmutex.Unlock()
 	kafkamdm.StartupGCPercent = percent.Value
 	ctx.PlainText(200, []byte("OK"))
 }
 
 func (s *Server) setNormalGCPercent(ctx *middleware.Context, percent models.NormalGCPercent) {
-	kafkamdm.GoGCmutex.lock()
+	kafkamdm.GoGCmutex.Lock()
 	defer kafkamdm.GoGCmutex.Unlock()
 	kafkamdm.NormalGCPercent = percent.Value
 	ctx.PlainText(200, []byte("OK"))
